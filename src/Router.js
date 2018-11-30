0db44eb01b825ced21b64df9ec86da21a9b7b95e
Avinash kumar <avinash2050kumar@gmail.com>
2018-11-30 14:41:10 +0530

    {Avinash}

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,17 @@
-import {DrawerNavigator} from 'react-navigation'
+import { DrawerNavigator } from "react-navigation";
 
-import TopHeadlines from './screen/TopHeadlines'
-import Everything from './screen/Everything'
-import Sources from './screen/Sources'
+import TopHeadlines from "./screen/TopHeadlines";
+import Everything from "./screen/Everything";
+import Sources from "./screen/Sources";
 
 export default DrawerNavigator({
-    TopHeadlines:{
-        screen: TopHeadlines
-    },
-    Everything: {
-        screen: Everything
-    },
-    Sources: {
-        screen: Sources
-    }
-})
+  TopHeadlines: {
+    screen: TopHeadlines
+  },
+  Everything: {
+    screen: Everything
+  },
+  Sources: {
+    screen: Sources
+  }
+});
